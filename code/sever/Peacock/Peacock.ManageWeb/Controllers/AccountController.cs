ae60ff6d00d5279676d85de14b72d0efe863a007
luwenbin <luwenbin@linshimuye.com>
2019-07-11 18:12:55 +0800

    添加登录页

---
+++ code/sever/Peacock/Peacock.ManageWeb/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Peacock.ManageWeb.Models;
+
+namespace Peacock.ManageWeb.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            LoginModel model = new LoginModel();
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (model.UserName.Equals("admin", StringComparison.InvariantCultureIgnoreCase) && model.Password.Equals("123"))
+            {
+                //登陆授权
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, model.UserName));
+                var indentity = new ClaimsIdentity(claims, "PeacockManageWeb");
+                var principal = new ClaimsPrincipal(indentity);
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    principal);
+
+                string redirectAction = Url.Action("Index");
+                if (!string.IsNullOrEmpty(HttpContext.Request.Query["ReturnUrl"].FirstOrDefault()))
+                {
+                    redirectAction = HttpContext.Request.Query["ReturnUrl"].FirstOrDefault();
+                }
+
+                return Redirect(redirectAction);
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect(Url.Action("Login"));
+        }
+    }
+}
--- code/sever/Peacock/Peacock.ManageWeb/Startup.cs
+++ code/sever/Peacock/Peacock.ManageWeb/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,6 +32,9 @@ namespace Peacock.ManageWeb
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(o => o.LoginPath = new PathString("/Account/Login"));
+
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -53,11 +57,13 @@ namespace Peacock.ManageWeb
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
+            app.UseAuthentication();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                    name: "default",
-                   template: "{controller=Home}/{action=Index}/{id?}");
+                   template: "{controller=Home}/{action=Demo}/{id?}");
             });
         }
     }
