0e2c99ea8a2e40659636052db1ea43df66b55ece
Ansens <ansenslu@live.com>
2019-08-12 23:46:19 +0800

    添加图片上传接口

--- code/sever/Peacock/Peacock.ManageWeb/Common/BaseController.cs
+++ code/sever/Peacock/Peacock.ManageWeb/Common/BaseController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Peacock.Dal;
 using Peacock.ViewModel.Manage;
@@ -42,6 +45,42 @@ namespace Peacock.ManageWeb
         {
             OperationResult operationResult = new OperationResult("-1", false, message, content);
             return operationResult;
+        }
+
+        public ActionResult GetUploadView()
+        {
+            return View("_Upload");
+        }
+
+        [HttpPost]
+        public ActionResult UploadImg([FromServices]IHostingEnvironment env, List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var fileBasePath = Path.Combine(env.WebRootPath, "upload", "img");
+            List<string> fileList = new List<string>();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    string fileExtension = formFile.FileName.Split('.').Last();
+                    string fileName = Guid.NewGuid().ToString().Replace("-", string.Empty) + "." + fileExtension;
+                    string fileFullPath = Path.Combine(fileBasePath, fileName);
+                    using (var stream = new FileStream(fileFullPath, FileMode.CreateNew))
+                    {
+                        //fileList.Add(fileName);
+                        fileList.Add(Request.Host + "/upload/img/" + fileName);
+                        formFile.CopyTo(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, fileList });
         }
     }
 }
