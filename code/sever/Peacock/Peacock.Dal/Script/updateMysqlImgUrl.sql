783dfad3dbeac5dfc86f0226ae6f9debb787e53b
luwenbin <luwenbin@linshimuye.com>
2019-09-27 14:51:49 +0800

    fix bug

--- code/sever/Peacock/Peacock.Dal/Script/updateMysqlImgUrl.sql
+++ code/sever/Peacock/Peacock.Dal/Script/updateMysqlImgUrl.sql
@@ -1,7 +1,39 @@
-﻿select @oldSever:='localhost:8901',@newSever:='192.168.1.107:8901';
+﻿select @oldSever:='localhost:8901',@newSever:='www.puwiremesh.com:2000';
 
 update T_Company
 set ImgPath = Replace(ImgPath,@oldSever,@newSever);
 
 update T_System_LanguageContent
 set DisplayContent = Replace(DisplayContent,@oldSever,@newSever);
+
+update t_pro_productimg
+set ImgUrl = Replace(ImgUrl,@oldSever,@newSever);
+
+update t_pro_product
+set ImgPath = Replace(ImgPath,@oldSever,@newSever);
+
+update t_pro_exampleimg
+set ImgUrl = Replace(ImgUrl,@oldSever,@newSever);
+
+update t_pro_example
+set ImgPath = Replace(ImgPath,@oldSever,@newSever);
+
+update t_new
+set ImgUrl = Replace(ImgUrl,@oldSever,@newSever);
+
+update t_advertise
+set ImgUrl = Replace(ImgUrl,@oldSever,@newSever);
+
+
+select * from T_Company;
+select * from T_System_LanguageContent;
+select * from t_pro_productimg;
+select * from t_pro_product;
+select * from t_pro_exampleimg;
+select * from t_pro_example;
+select * from t_new;
+select * from t_advertise;
+
+
+
+
--- code/sever/Peacock/Peacock.ManageWeb/Controllers/UserController.cs
+++ code/sever/Peacock/Peacock.ManageWeb/Controllers/UserController.cs
@@ -93,7 +93,7 @@ namespace Peacock.ManageWeb.Controllers
                 return PartialView("Edit", model);
             }
 
-            bool isExists = peacockDbContext.T_User.Any(i => i.UserName == model.UserName);
+            bool isExists = peacockDbContext.T_User.Count(i => i.UserName == model.UserName) > 0;
             if (isExists)
             {
                 ModelState.AddModelError("UserName", "用户名已存在");
@@ -104,7 +104,7 @@ namespace Peacock.ManageWeb.Controllers
             {
                 UserName = model.UserName,
                 Password = BaseIdentityService.HashPassword(model.Password),
-                Status = true,
+                Status = false,
                 CreatedTime = DateTime.Now,
                 LastUpdatedTime = DateTime.Now,
             };
